0b56f227e4413a9d48d7b4f91fb6f28ccb8301f7
Code Queen <channguyen97@gmail.com>
2023-10-29 13:50:48 -0700

    added to resolvers files the queries and mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+const { User } = require('../models');
+const { signToken, AuthentificationError } = require('../utils/auth'); //import signToken from auth
+
+const resolvers = {
+    Query: {
+        // ????????? find user by id or by username
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ 
+                    $or: [{ _id: context.user._id }, { username: context.user.username }], 
+                }); 
+            }
+            throw AuthentificationError;
+        },
+    },
+//create user, server signs a token, and sends it back to the client
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user); //create token with the user's args/info
+
+            return { token, user };
+
+//logs in a user, server signs a token, and sends it back to the client
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthentificationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthentificationError;
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+  // user comes from `req.user` created in the auth middleware function
+        saveBook: async (parent, { savedBooks, description, title, bookId, link }, context) => {
+        try {
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id }, //find by the user._id - include user._id in the body
+            { $addToSet: { savedBooks: description, title, bookId, link } }, //pushes to savedBooks array these properties
+            { new: true,
+            runValidators: true }
+        );
+        return updatedUser;
+        } catch (err) {
+            throw AuthentificationError;
+        }
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id }, //find by the user._id - include user._id in the body
+                    { $pull: { savedBooks: { bookId: bookId } } }, //pull from savedBooks array, by its bookId
+                    { new: true }
+                );
+            }
+            throw AuthentificationError;
+        }
+    },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,5 @@
-// typeDefs.js: Define the necessary Query and Mutation types:
+// typeDefs.js: Define the necessary Query and Mutation 
+
 // User type:
 // _id
 // username
@@ -14,19 +15,18 @@
 // image
 // link
 
+// Auth type:
+// token
+// user (References the User type.)
+
 // type Query:
 // me: User
 
 // type Mutation:
-
 // login: Accepts an email and password as parameters; returns an Auth type.
-
 // addUser: Accepts a username, email, and password as parameters; returns an Auth type.
-
 // saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
-
 // removeBook: Accepts a book's bookId as a parameter; returns a User type.
-
 
 const typeDefs = `
 type User {
@@ -56,9 +56,9 @@ me: User
 }
 
 type Mutation {
-login(email: String!, password: String!): Auth
 addUser(username: String!, email: String!, password: String!): Auth
-saveBook(savedBooks: Book!, description: String!, title: String!, bookId: ID!, link: String!): User
+login(email: String!, password: String!): Auth
+saveBook(savedBooks: [Book], description: String!, title: String!, bookId: ID!, link: String!): User
 removeBook(bookId: ID!): User
 }
 
