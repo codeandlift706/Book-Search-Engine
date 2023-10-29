13da1bcb1f31c571aa14e213a14ee0d8bacdfdfb
Code Queen <channguyen97@gmail.com>
2023-10-29 12:06:11 -0700

    added typeDefs for user, book, queries, mutations

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,65 @@
+// typeDefs.js: Define the necessary Query and Mutation types:
+// User type:
+// _id
+// username
+// email
+// bookCount
+// savedBooks (This will be an array of the Book type.) Clarify what the ! means
+
+// Book type:
+// bookId (Not the _id, but the book's id value returned from Google's Book API.)
+// authors (An array of strings, as there may be more than one author.)
+// description
+// title
+// image
+// link
+
+// type Query:
+// me: User
+
+// type Mutation:
+
+// login: Accepts an email and password as parameters; returns an Auth type.
+
+// addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+
+// saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+
+// removeBook: Accepts a book's bookId as a parameter; returns a User type.
+
+
+const typeDefs = `
+type User {
+_id: ID
+username: String
+email: String
+bookCount: 
+savedBooks: [Book]
+}
+
+type Book {
+bookId: ID!
+authors: [String]
+description: String
+title: String
+IMage: String
+Link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+me: User
+}
+
+type Mutation {
+login(email: String!, password: String!): Auth
+addUser(username: String!, email: String!, password: String!): Auth
+saveBook(savedBooks: Book!, description: String!, title: String!, bookId: ID!, link: String!): User
+removeBook(bookId: ID!): User
+}
+
+`
