9ac8534df05bb9bf894e3c5ed50999d84e721f2e
Code Queen <channguyen97@gmail.com>
2023-10-29 11:35:54 -0700

    added to auth file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,5 @@
+const { GraphQLError } = require('graphql'); //so that we can through Auth error - in lieu of telling the user specifically what the error is
+
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,10 +7,16 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthentificationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNATHENTICATED',
+    }
+  }),
+
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }, res, next) { //destructure req so we have access to client req, retrieve from it the info needed to create the token
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization; // 'Bear <token>'
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -29,7 +37,8 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    next(); //where is this next endpoint?
+    return req; //return the req object so it can be passed to resolver as "context"
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
