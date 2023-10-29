3fbb4300b595dc92dc839c638b8b804ce6bf28d1
Code Queen <channguyen97@gmail.com>
2023-10-29 15:07:13 -0700

    switched mutations and queries into correct files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,52 +1,22 @@
-// LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
-// ADD_USER will execute the addUser mutation.
-// SAVE_BOOK will execute the saveBook mutation.
-// REMOVE_BOOK will execute the removeBook mutation.
+// queries.js: This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
 
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
     }
-}
+  }
 `;
 
-export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
-    login(username: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
-    }
-}
-`;
-
-export const SAVE_BOOK = gql`
-mutation saveBook($savedBooks: [Book], $description: String!, $title: String!, $bookId: ID!, $link: String!) {
-    saveBook(savedBooks: [Book], description: $description, title: $title, bookId: $bookId, link: $link) {
-        user {
-            _id
-            username
-            savedBooks
-        }
-    }
-}
-`;
-
-export const REMOVE_BOOK = gql`
-mutation deleteBook($bookId: ID!) {
-    deleteBook(bookId: $bookId) {
-        _id
-        username
-        savedBooks
-    }
-}
-`;
