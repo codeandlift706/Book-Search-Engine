9dc4a4e728921bba08e0b356eb7cf634e4a932e7
Code Queen <channguyen97@gmail.com>
2023-10-29 19:01:12 -0700

    added to loginform page

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,8 +1,9 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { LOGIN_USER } from '../utils/mutations'
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -28,15 +29,16 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: {...userFormData},
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { ADD_USER } from '../utils/mutations'
 import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations'
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
